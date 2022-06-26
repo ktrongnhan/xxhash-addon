8b07e91ab5f36bb6eaa27a7ac3cb01128e3c20a3
NK <knhana7@gmail.com>
2022-06-26 13:22:04 +0100

    fix CIs

--- benchmark.js
+++ benchmark.js
@@ -4,14 +4,14 @@ const { createHash, randomFillSync } = require('crypto');
 const { Buffer } = require('buffer');
 const { PerformanceObserver, performance } = require('perf_hooks');
 
-const major_ver = process.versions.node.split('.')[0];
-const platform = process.platform;
+// const major_ver = process.versions.node.split('.')[0];
+// const platform = process.platform;
 
-// Windows perf_hooks fails on CI with Node 8. Likely because perf_hooks was
-// still experimental at that time.
-if (major_ver === '8' && platform === 'win32') {
-   process.exit();
-}
+// // Windows perf_hooks fails on CI with Node 8. Likely because perf_hooks was
+// // still experimental at that time.
+// if (major_ver === '8' && platform === 'win32') {
+//    process.exit();
+// }
 
 // Init input, a 1GB buffer randomly filled.
 const buf = Buffer.alloc(2 ** 30);
@@ -22,7 +22,7 @@ randomFillSync(buf_seed);
 
 const md5 = process.env.DEBUG ? null : createHash('md5');
 const sha1 = process.env.DEBUG ? null : createHash('sha1');
-const blake = process.env.DEBUG ? null : createHash('blake2s256');
+// const blake = process.env.DEBUG ? null : createHash('blake2s256');
 
 const xxh64 = new XXHash64(buf_seed);
 const xxh3 = new XXHash3(buf_seed);
@@ -38,10 +38,11 @@ const obs = new PerformanceObserver(items => {
 obs.observe({ entryTypes: ['measure'] });
 
 for (let i = 0; i < hashes.length; i++) {
-   performance.mark('start');
+   performance.mark('start - ' + names[i]);
    for (let j = 0; j < 10; j++) {
       hashes[i].update(buf);
    }
    hashes[i].digest();
-   performance.measure(names[i], 'start');
+   performance.mark('end - ' + names[i]);
+   performance.measure(names[i], 'start - ' + names[i], 'end - ' + names[i]);
 };
