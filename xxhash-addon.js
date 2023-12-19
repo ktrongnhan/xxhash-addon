38c8e4688998f5076859ba2e6e734832df160b87
Tamás Sallai <82075+sashee@users.noreply.github.com>
2023-12-19 03:40:24 +0100

    Auto-discover Release and Debug builds (#31)

--- xxhash-addon.js
+++ xxhash-addon.js
@@ -1,3 +1,8 @@
-module.exports = {
-   ...require(process.env.DEBUG === undefined ? './build/Release/addon' : './build/Debug/addon')
-};
+try {
+  var addon = require('./build/Release/addon');
+} catch (e) {
+  if (e.code !== 'MODULE_NOT_FOUND') throw e;
+  var addon = require('./build/Debug/addon');
+}
+
+module.exports = addon;
